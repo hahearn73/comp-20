6503c74d2b2eeb346e6e1ed1ad26bf5a802fbac4
hahearn73 <98117757+hahearn73@users.noreply.github.com>
2022-04-07 14:30:13 -0400

    Add files via upload

---
+++ products_and_prices.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE jade2;
+CREATE TABLE products (
+	name varchar(255),
+	price int
+);
+INSERT INTO jade2 (name, price)
+VALUES ('Chicken Chop Suey', 4.5),
+       ('Sweet and Sour Pork', 6.25),
+       ('Shrimp Lo Mein', 6.25),
+       ('Moo Shi Chicken', 7.5),
+       ('Fried Rice', 2.85);
